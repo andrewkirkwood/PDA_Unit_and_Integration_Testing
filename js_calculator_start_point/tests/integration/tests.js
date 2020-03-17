ff85f238f45773cc4ce9867491f84e98077fc726
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-03-17 16:52:34 +0000

    first 4 UI integration tests passing

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,4 +16,34 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
+  it('number buttons should update the display of the running total', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number6')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('26')
+  })
+
+
+  it('arithmetical operations update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add ')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals ')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  })
+
+  it('multiple operations can be chained together and the display updates correctly', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_add ')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply ')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals ')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('36')
+  })
+
+
 });
